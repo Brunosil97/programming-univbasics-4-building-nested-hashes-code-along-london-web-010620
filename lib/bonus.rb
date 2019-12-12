a71fde29efa74fdb2e7ff7686515e6f3d8a196e3
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-12 14:22:36 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -28,3 +28,4 @@ epic_tragedy[:capulet][:heroine][:status] = "Dead"
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+epic_tragedy
